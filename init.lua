e9b6c042014c787bb35ccfa1723d9cdc3d99f55c
LuizEduardoCardozo <eduard.cardoz@gmail.com>
2023-09-12 13:36:37 -0300

    add main plugins and configs

---
+++ init.lua
@@ -0,0 +1,9 @@
+require('lazy_config')
+require('mason_config')
+require('lualune_config')
+
+require('coc_config')
+
+require('options')
+
+require('theme')
---
+++ lua/plugins/plugins.lua
@@ -0,0 +1,35 @@
+return {
+	{ "williamboman/mason.nvim" },
+	{ "neovim/nvim-lspconfig" },
+	{ "simrat39/rust-tools.nvim" },
+	{ "nvim-lua/plenary.nvim" },
+	{ "mfussenegger/nvim-dap" },
+	{ "airblade/vim-gitgutter" },
+  { "mg979/vim-visual-multi" },
+	{ "nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" } },
+	{ "L3MON4D3/LuaSnip" },
+	{ "simrat39/rust-tools.nvim" },
+  { "folke/trouble.nvim" },
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = function()
+      return {
+        { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+        { "<leader>fs", "<cmd>Telescope grep_string<cr>", desc = "Find Strings" },
+      }
+    end,
+    dependencies = { 'nvim-lua/plenary.nvim', 'BurntSushi/ripgrep' }
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "moon" },
+  },
+  {
+    "onsails/lspkind.nvim"
+  },
+  {
+    'neoclide/coc.nvim',
+    branch='release'
+  }
+}
